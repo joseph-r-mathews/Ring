Fixed train loop bug regarding backprop failure

ring_virtual_tryon/diffusion.py
@@ -122,7 +122,8 @@ def train_step(model, vae, scheduler,optimizer,
 
 # optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
-# image_file_path="/Users/jm/Downloads/Data"
+# #"/Users/jm/Downloads/Data"
+# image_file_path=r"C:\Users\Joe\Desktop\Data"
 
 # data = DataLoader(CachedRingLatents(image_file_path, vae), batch_size, shuffle)
 # # --- Encode Prompt ---
@@ -136,9 +137,17 @@ def train_step(model, vae, scheduler,optimizer,
 #     return_tensors="pt",
 # )
 # text_input_ids = text_inputs.input_ids.to(device)
-# text_embeds = text_encoder(text_input_ids).last_hidden_state
+
+# with torch.no_grad():
+#     text_embeds = text_encoder(text_input_ids).last_hidden_state
+
+# text_embeds = text_embeds.detach()
 
 # condition_features, x0 = next(iter(data))
+# t = torch.randint(0, scheduler.config.num_train_timesteps, (batch_size,), device=device)
 
-# train_step(model, vae, scheduler, loss_fn_diffusion,loss_fn_img, x0, 100, text_embeds ,
-#            condition_features, optimizer, device)
+# train_step(
+#                 model, vae, scheduler, optimizer, 
+#                 loss_fn_diffusion, loss_fn_img,
+#                 x0, t, text_embeds , condition_features, device
+#             )

ring_virtual_tryon/train.py
@@ -1,7 +1,7 @@
-from dataset import CachedRingLatents
+from .dataset import CachedRingLatents
 from torch.utils.data import DataLoader
 import torch
-from diffusion import train_step
+from .diffusion import train_step
 
 # importlib.reload(diffusion)
 
@@ -46,7 +46,11 @@ def train_loop(model, vae, tokenizer, text_encoder, scheduler, optimizer,
         return_tensors="pt",
     )
     text_input_ids = text_inputs.input_ids.to(device)
-    text_embeds = text_encoder(text_input_ids).last_hidden_state # (1, 77, 768)
+    
+    with torch.no_grad():
+        text_embeds = text_encoder(text_input_ids).last_hidden_state
+    
+    text_embeds = text_embeds.detach() # (1, 77, 768)
 
     for epoch in range(nepochs):
         epoch_loss = 0.0

run.py
@@ -1,4 +1,3 @@
-
 import torch
 import torch.optim as optim
 # From other modules
@@ -15,13 +14,11 @@ loss_fn_img = torch.nn.L1Loss()
 optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
 # ------------------------------ Training Call ----------------------------------
-num_timesteps = 1000        
-batch_size = 1                    
 
 train_loop(model, vae, tokenizer, text_encoder, scheduler, optimizer, 
            loss_fn_diffusion, loss_fn_img,
-           batch_size = 1, shuffle=True, nepochs = 1, device = None, 
-           image_file_path="/Users/jm/Downloads/Data")
+           batch_size = 1, shuffle=True, nepochs = 30, device = None, 
+           image_file_path=r"C:\Users\Joe\Desktop\Data")
 
 torch.save(model.state_dict(), "checkpoints/main_unet.pth")
 print("✅ Model saved to checkpoints/main_unet.pth")